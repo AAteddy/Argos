c3b89c3cdb385f7700771bcc540dd20f9e343939
Tedros Alem <tedsaasfaha@gmail.com>
2024-02-29 12:34:37 +0300

    db initialized in to flask app main.py and Server db created with tables id, hostname, server_username, server_password, port

--- server_flask/main.py
+++ server_flask/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_restx import Api, Resource, fields
 from config import DevConfig
+from models import Server
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
+db.init_app(app)
+
 api = Api(app, doc="/docs")
 
 
@@ -11,6 +16,11 @@ api = Api(app, doc="/docs")
 class HelloWorld(Resource):
     def get(self):
         return {"message": "Hello World"}
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "Server": Server}
 
 
 if __name__ == "__main__":
