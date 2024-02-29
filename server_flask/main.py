0f0aef803739341fdf8cacae2538ee57a01fa0d2
Tedros Alem <tedsaasfaha@gmail.com>
2024-02-29 13:45:52 +0300

    Get and Post api for servers created and tested

--- server_flask/main.py
+++ server_flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Server
@@ -12,10 +12,52 @@ db.init_app(app)
 api = Api(app, doc="/docs")
 
 
+# server model (serializer)
+server_model = api.model(
+    "Server",
+    {
+        "id": fields.Integer(),
+        "hostname": fields.String(),
+        "server_username": fields.String(),
+        "server_password": fields.String(),
+        "port": fields.String(),
+    },
+)
+
+
 @api.route("/hello")
 class HelloWorld(Resource):
     def get(self):
         return {"message": "Hello World"}
+
+
+@api.route("/servers")
+class ServersResource(Resource):
+
+    @api.marshal_list_with(server_model)
+    def get(self):
+        """Get all remote servers"""
+
+        servers = Server.query.all()
+
+        return servers
+
+    @api.marshal_with(server_model)
+    def post(self):
+        """Create a new remote server"""
+
+        data = request.get_json()
+
+        new_server = Server(
+            hostname=data.get("hostname"),
+            server_username=data.get("server_username"),
+            server_password=data.get("server_password"),
+            port=data.get("port"),
+        )
+
+        new_server.save()
+
+        return new_server, 201
 
 
 @app.shell_context_processor
