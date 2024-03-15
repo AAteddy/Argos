90e909ce22e25dc865b223adae03c16896b6b19b
Tedros Alem <tedsaasfaha@gmail.com>
2024-03-15 21:26:17 +0300

    unittest test_get_a_server() for getting a particular server is fixed.

--- backend_flask/test_api.py
+++ backend_flask/test_api.py
@@ -178,46 +178,46 @@ class APITestCase(unittest.TestCase):
 
         self.assertEqual(status_code, 200)
 
-    # def test_get_a_server(self):
-    #     """Test getting a specific server by its ID"""
-    #     id = 1
+    def test_get_a_server(self):
+        """Test getting a specific server by its ID"""
+        id = 1
 
-    #     signup_response = self.client.post(
-    #         "/auth/signup",
-    #         json={
-    #             "username": "testuser",
-    #             "email": "testuser@testmail.com",
-    #             "password": "password",
-    #         },
-    #     )
+        signup_response = self.client.post(
+            "/auth/signup",
+            json={
+                "username": "testuser",
+                "email": "testuser@testmail.com",
+                "password": "password",
+            },
+        )
 
-    #     login_response = self.client.post(
-    #         "/auth/login",
-    #         json={"email": "testuser@testmail.com", "password": "password"},
-    #     )
+        login_response = self.client.post(
+            "/auth/login",
+            json={"email": "testuser@testmail.com", "password": "password"},
+        )
 
-    #     access_token = login_response.json["access_token"]
+        access_token = login_response.json["access_token"]
 
-    #     create_server_response = self.client.post(
-    #         "/server/",
-    #         json={
-    #             "title": "Apache-Test-Server",
-    #             "hostname": "test-server",
-    #             "server_username": "test@server",
-    #             "server_password": "test123",
-    #             "port": "22",
-    #         },
-    #         headers={"Authorization": f"Bearer {access_token}"},
-    #     )
+        create_server_response = self.client.post(
+            "/server/",
+            json={
+                "title": "Test-Server-1",
+                "hostname": "62468bd2f6a3.e336d92f.alx-cod.online",
+                "server_username": "62468bd2f6a3",
+                "server_password": "3bca7fb8e251461e68e2",
+                "port": "22",
+            },
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
-    #     get_server_response = self.client.get(
-    #         f"/server/{id}",
-    #         headers={"Authorization": f"Bearer {access_token}"},
-    #     )
+        get_server_response = self.client.get(
+            f"/server/{id}",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
-    #     status_code = get_server_response.status_code
+        status_code = get_server_response.status_code
 
-    #     self.assertEqual(status_code, 200)
+        self.assertEqual(status_code, 200)
 
     # def test_update_server(self):
     #     """Test updating an existing server."""
