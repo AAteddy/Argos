7d96adb09f54049153cf2b90505ddc21c0b713a0
Tedros Alem <tedsaasfaha@gmail.com>
2024-04-04 11:13:54 +0300

    Navbar component recreated with new theme

--- client_react/src/components/landing_page/HeroSection.tsx
+++ client_react/src/components/landing_page/HeroSection.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+// import React from "react";
 
-const HeroSection = () => {
-  return <div>HeroSection</div>;
-};
+// const HeroSection = () => {
+//   return <div>HeroSection</div>;
+// };
 
-export default HeroSection;
+// export default HeroSection;
