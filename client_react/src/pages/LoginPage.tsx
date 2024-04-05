4225f67be279e1dc9c9b44b3484eef9ecdf5ef56
Tedros Alem <tedsaasfaha@gmail.com>
2024-04-05 16:18:36 +0300

    LoginPage form, form validation implemented

--- client_react/src/pages/LoginPage.tsx
+++ client_react/src/pages/LoginPage.tsx
@@ -11,15 +11,58 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     email: data.get("email"),
+  //     password: data.get("password"),
+  //   });
+
+  const form = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = form;
+
+  const navigate = useNavigate();
+
+  const submitForm = (data: FormValues) => {
+    const body = {
+      email: data.email,
+      password: data.password,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    };
+
+    fetch("http://127.0.0.1:5000/auth/login", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+
+    navigate("/dashboard");
+    reset();
   };
 
   return (
@@ -39,26 +82,54 @@ const LoginPage = () => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(submitForm)}
+          noValidate
+          sx={{ mt: 1 }}
+        >
           <TextField
             margin="normal"
-            required
             fullWidth
             id="email"
             label="Email Address"
-            name="email"
+            type="email"
             autoComplete="email"
-            autoFocus
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              maxLength: {
+                value: 25,
+                message: "Characters over limit",
+              },
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Please enter a valid email address",
+              },
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="password"
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
+            autoComplete="new-password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 4,
+                message: "Must be at least 4 characters long",
+              },
+            })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
